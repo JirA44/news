07e4c94c0fc91183669b489d80c29eb447f8256e
Majira <JirA44@users.noreply.github.com>
2026-06-11 10:37:16 +0200

    fix: resolve #69748 [RegisterSec] Chrome's zero-day Whac-A-Mole contin

--- _python/config.py
+++ _python/config.py
@@ -1,1 +1,11 @@
-# local config only
+name: "fundamental_issue"
+on:
+  push:
+    branches:
+      - master
+jobs:
+  fundamental_issue:
+    runs-on: ubuntu-latest
+    steps:
+      - name: "Run code"
+        script: python3.10 solution.py
